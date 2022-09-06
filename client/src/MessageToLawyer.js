17cd19b98d1a27f7aa699a6bf2c230edf0617599
Can Le <canle360@gmail.com>
2022-09-06 11:31:42 -0600

    add time to string key value to message post object

--- client/src/MessageToLawyer.js
+++ client/src/MessageToLawyer.js
@@ -37,6 +37,7 @@ const MessageToLawyer = ({}) => {
       receiverId: specificLawyer._id,
       ...values,
       time: Date.now(),
+      timeToString: Date(Date.now()).toString(),
       //  this is for both when a client click on a specific lawyer page or click on message to see who sent that message
     };
     try {
