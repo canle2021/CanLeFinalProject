8186ff0ecab296ef879552e4397c468a9d7304fd
Can Le <canle360@gmail.com>
2022-09-13 12:30:24 -0600

    lawyers list -homePage styles

--- client/src/HomePage.js
+++ client/src/HomePage.js
@@ -20,35 +20,42 @@ const HomePage = () => {
   }, []);
   return (
     <HomePageDiv>
-      <h1>HOME PAGE</h1>
-
+      <h1>MEET OUR TEAM</h1>
       <Team>
         {allLawyersPicture.map((picture, index) => {
           return (
-            <Link to={`/lawyer/${picture._id}`} key={index}>
-              <LawyerpictureDiv>
-                <Picture src={picture.picture}></Picture>
-                <Name>
-                  {allLawyersInformation.map((eachLawyer, index) => {
-                    if (eachLawyer._id === picture._id) {
-                      return (
-                        <span key={index}>
-                          {eachLawyer.firstName} {eachLawyer.lastName}, Lawyer
-                        </span>
-                      );
-                    }
-                  })}
-                </Name>
-              </LawyerpictureDiv>
-            </Link>
+            <Box>
+              <LinkToProfile to={`/lawyer/${picture._id}`} key={index}>
+                <LawyerpictureDiv>
+                  <Picture src={picture.picture}></Picture>
+                  <Name>
+                    {allLawyersInformation.map((eachLawyer, index) => {
+                      if (eachLawyer._id === picture._id) {
+                        return (
+                          <span key={index}>
+                            {eachLawyer.firstName} {eachLawyer.lastName}, Lawyer
+                          </span>
+                        );
+                      }
+                    })}
+                  </Name>
+                </LawyerpictureDiv>
+              </LinkToProfile>
+            </Box>
           );
         })}
       </Team>
     </HomePageDiv>
   );
 };
-const Name = styled.p``;
-const Picture = styled.img``;
+
+const LinkToProfile = styled(Link)`
+  text-decoration: none;
+`;
+const Picture = styled.img`
+  width: 100%;
+  margin: 0;
+`;
 const LawyerpictureDiv = styled.div`
   display: flex;
   flex-direction: column;
@@ -56,11 +63,48 @@ const LawyerpictureDiv = styled.div`
 `;
 const Team = styled.div`
   flex-wrap: wrap;
-  gap: 40px 40px;
+  gap: 60px 50px;
   width: 1280px;
   display: flex;
   justify-content: center;
 `;
 const HomePageDiv = styled.div``;
+const Box = styled.div`
+  width: 280px;
+  height: fit-content;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  border-radius: 10px;
+  overflow: hidden;
+  box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+  :hover& {
+    cursor: pointer;
+    box-shadow: rgba(51, 2, 251, 0.8) 0px 4px 16px 10px;
+    transition: 0.5s ease-in-out;
+  }
+`;
+const Name = styled.button`
+  font-family: Georgia, serif;
+  width: 100%;
+  height: 80px;
+  background: transparent;
 
+  border: none;
+  margin: 0;
+  font-size: 18px;
+  text-transform: uppercase;
+  font-weight: 600;
+  color: rgba(27, 15, 79, 0.8);
+  background: linear-gradient(
+    90deg,
+    rgba(0, 54, 245, 0.8) 0%,
+    rgba(160, 54, 245, 0.8) 100%
+  );
+  :hover& {
+    cursor: pointer;
+    transition: 0.5s ease-in-out;
+    color: white;
+  }
+`;
 export default HomePage;
